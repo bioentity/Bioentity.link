f839056fbf8474e568f692287670899165b9be86
nickstiffler <nicholasstiffle@gmail.com>
2020-02-13 18:13:56 -0800

    improved linking modal

--- client/src/app/markup/markup-modal.component.ts
+++ client/src/app/markup/markup-modal.component.ts
@@ -1,18 +1,19 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
-import {UUID} from 'angular2-uuid';
+import { UUID } from 'angular2-uuid';
 
-import {Markup} from './markup';
-import {MarkupService} from './markup.service';
-import {LexiconService} from '../lexicon/lexicon.service';
-import {LexiconSource} from '../lexicon/lexicon-source';
-import {Lexicon} from '../lexicon/lexicon';
-import {KeyWordService} from '../key-word/key-word.service';
-import {KeyWordSet} from '../key-word/key-word-set';
-import {PublicationService} from "../publication/publication.service";
-import {StatisticsService} from "../publication/statistics/statistics.service";
+import { Markup } from './markup';
+import { MarkupService } from './markup.service';
+import { LexiconService } from '../lexicon/lexicon.service';
+import { LexiconSource } from '../lexicon/lexicon-source';
+import { Lexicon } from '../lexicon/lexicon';
+import { KeyWordService } from '../key-word/key-word.service';
+import { KeyWordSet } from '../key-word/key-word-set';
+import { PublicationService } from "../publication/publication.service";
+import { StatisticsService } from "../publication/statistics/statistics.service";
+import { Publication } from 'app/publication/publication';
 
 @Component({
     selector: 'markup-modal',
@@ -21,7 +22,7 @@ import {StatisticsService} from "../publication/statistics/statistics.service";
 })
 export class MarkupModal {
     @Input() markup;
-	@Input() doi;
+    @Input() publication: Publication
     @Output() markupChanged = new EventEmitter<string>();
     lexiconSources: LexiconSource[];
     isCollapsed = true;
@@ -30,16 +31,51 @@ export class MarkupModal {
     reason: string;
     curatorComments: string;
     @Input() isSaved;
+    notFound: boolean;
+    @Input() extLinkId: string;
+    notIndexed: boolean;
+    isNewLexicon: boolean;
+    isChecking: boolean;
 
-    constructor(public activeModal: NgbActiveModal
-        , private lexiconService: LexiconService
-        , private markupService: MarkupService
-        , private keyWordService: KeyWordService
+    constructor(public activeModal: NgbActiveModal,
+        private lexiconService: LexiconService,
+        private markupService: MarkupService,
+        private keyWordService: KeyWordService,
+        private publicationService: PublicationService
     ) {
     }
 
     ngOnInit() {
-        this.getLexiconSources();
+        if (this.isSaved) {
+            this.markupService.getMarkupsForExtLinkId(this.extLinkId).subscribe(markup => {
+                console.log("got it")
+                this.markup = markup
+                console.log(this.markup)
+
+                //if (this.markup && this.markup.keyWordSet) {
+                this.isCollapsed = false;
+                // this.isSaved = true
+                // }
+            }, error => {
+                console.log("not found")
+                this.notFound = true
+            }
+
+
+            )
+        } else {
+            this.isCollapsed = true
+            // if(!this.notFound) {
+            this.getLexiconSources();
+            // }
+            this.publicationService.getIndices(this.markup.publication).subscribe(indices => {
+                if (indices.indexOf(this.markup.keyWord.value) == -1) {
+                    this.notIndexed = true
+                }
+            })
+            //console.log(this.markup.publication)
+            //this.selectedSource = this.markup.keyWordSet.lexiconSources[0]
+        }
     }
 
 
@@ -51,18 +87,36 @@ export class MarkupModal {
             this.lexiconSources = applicationData;
         });
 
-        console.log(this.markup)
-        if (this.markup.keyWordSet) {
-            this.isCollapsed = false;
-        }
+        //console.log(this.markup)
+
+    }
+
+    checkModId() {
+        this.isChecking = true;
+        this.lexiconService.getLexiconByModId(this.externalModId).subscribe(lexicon => {
+            this.isNewLexicon = false
+            if (!lexicon.error) {
+                this.lexiconSources.forEach(lexiconSource => {
+                    if (lexiconSource.id == lexicon.lexiconSource.id) {
+                        this.selectedSource = lexiconSource
+                    }
+                })
+            } else {
+                this.selectedSource = null;
+                this.isNewLexicon = true;
+            }
+            this.isChecking = false;
+        })
     }
 
     saveLexicon(linkAll) {
+                            this.isCollapsed = false;
+
         let lexicon = new Lexicon();
         lexicon.publicName = this.markup.keyWord.value;
         lexicon.externalModId = this.externalModId;
         lexicon.lexiconSource = this.selectedSource;
-//        lexicon.getLink();
+        //        lexicon.getLink();
         lexicon.link = "https://identifiers.org/bioentitylink/" + this.selectedSource.prefix + ":" + this.externalModId;
         lexicon.curatorNotes = this.curatorComments;
         lexicon.getInternalLink();
@@ -82,7 +136,7 @@ export class MarkupModal {
                 this.markup.keyWord = applicationData;
                 // For some reason, lexica is null here
                 this.markup.keyWord.lexica = lexicon;
-                
+
                 // Save to server
                 let lexObject = {
                     selection: {
@@ -97,6 +151,7 @@ export class MarkupModal {
                 };
                 this.markupService.saveLink(lexObject, this.markup.publication.fileName.substr(0, this.markup.publication.fileName.length - 4)).subscribe(applicationData => {
                     if (linkAll) {
+
                         this.applyKeyWord();
                     }
                 });
@@ -106,10 +161,10 @@ export class MarkupModal {
 
         //let link = this.selectedSource.urlConstructor.replace("@@ID@@", this.externalModId);
         // let link = "https://bioentity.link/#/lexicon/public/" + this.selectedSource.prefix + ":" + this.externalModId + "?doi=" + this.markup.publication.doi;
-        let link = this.markupService.generateLink(this.selectedSource.prefix,this.externalModId,this.markup.publication.doi);
-        let terms = {extLinkId: this.markup.extLinkId, link: link};
+        let link = this.markupService.generateLink(this.selectedSource.prefix, this.externalModId, this.markup.publication.doi);
+        let terms = { extLinkId: this.markup.extLinkId, link: link };
         // Save to publication
-        window.frames[0].postMessage({action: 'updateLink', terms: terms}, "*");
+        window.frames[0].postMessage({ action: 'updateLink', terms: terms }, "*");
         this.markupChanged.emit('update-link');
         this.isSaved = true;
         this.isCollapsed = true;
@@ -120,22 +175,22 @@ export class MarkupModal {
         this.markup.keyWord.lexica = [this.markup.keyWord.lexica];
         let wordData = [this.markup.keyWord];
         console.log(wordData);
-        window.frames[0].postMessage({action: 'linkPub', publication: this.markup.publication, terms: wordData}, "*");
+        window.frames[0].postMessage({ action: 'linkPub', publication: this.markup.publication, terms: wordData }, "*");
         this.markupChanged.emit('add-keywords');
     }
 
     closeModal() {
         if (!this.isSaved) {
             // Delete link in pub
-            window.frames[0].postMessage({action: 'deleteExtLink', terms: {extLinkId: this.markup.extLinkId}}, "*");
+            window.frames[0].postMessage({ action: 'deleteExtLink', terms: { extLinkId: this.markup.extLinkId } }, "*");
         }
         this.activeModal.close();
     }
 
     deleteLink() {
         this.markupService.deleteMarkup(this.markup).subscribe(applicationData => {
-//            window.frames[0].postMessage({action: 'deleteExtLink', terms: {extLinkId: this.markup.extLinkId}}, "*");
-            window.frames[0].postMessage({action: 'deleteExtLink', terms: {extLinkId: this.markup.extLinkId, paragraph: this.markup.path}}, "*");
+            //            window.frames[0].postMessage({action: 'deleteExtLink', terms: {extLinkId: this.markup.extLinkId}}, "*");
+                  window.frames[0].postMessage({action: 'deleteExtLink', terms: {extLinkId: this.markup.extLinkId, paragraph: this.markup.path}}, "*");
             this.markupChanged.emit('delete-link');
             this.activeModal.close();
         });
@@ -143,21 +198,30 @@ export class MarkupModal {
 
     deleteAllLinks() {
         this.markupService.getMarkupsForKeyWord(this.markup.keyWord, this.markup.publication).subscribe(markups => {
-            console.log('delete markups: '+markups.length)
+            console.log('delete markups: ' + markups.length)
             let markupIds = [];
-            for(let m of markups){
+            for (let m of markups) {
                 markupIds.push(m.uuid);
             }
 
             this.markupService.deleteAllMarkups(markupIds).subscribe(applicationData => {
+                let extLinkIds = []
                 for (let markup of markups) {
-       //                 window.frames[0].postMessage({action: 'deleteExtLink', terms: {extLinkId: markup.extLinkId}}, "*");
-                    window.frames[0].postMessage({action: 'deleteExtLink', terms: {extLinkId: markup.extLinkId, paragraph: markup.path}}, "*");
+                    //                 window.frames[0].postMessage({action: 'deleteExtLink', terms: {extLinkId: markup.extLinkId}}, "*");
+                    //console.log(markup)
+                    extLinkIds.push(markup.extLinkId)
                 }
+                    window.frames[0].postMessage({ action: 'deleteAllLinks', terms: { extLinkIds: extLinkIds } }, "*");
+                //}
                 this.markupChanged.emit('delete-all-links');
                 this.activeModal.close();
             });
         });
+    }
+
+    deleteMissingLink() {
+        window.frames[0].postMessage({ action: 'deleteExtLink', terms: { extLinkId: this.extLinkId } }, "*");
+        this.activeModal.close();
     }
 }
 
