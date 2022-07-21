ceaa2066d8e0bfabca8538af7533cb65aec45793
nickstiffler <nicholasstiffle@gmail.com>
2022-07-21 11:10:06 -0700

    fixes for oup

--- texture/lib/jats/contrib-group/ContribGroupConverter.js
+++ texture/lib/jats/contrib-group/ContribGroupConverter.js
@@ -1,31 +1,46 @@
-import XMLIterator from '../../util/XMLIterator'
+import XMLIterator from "../../util/XMLIterator";
 
-let CONTRIB_GROUP = ['contrib', 'address', 'aff', 'aff-alternatives', 'author-comment', 'bio', 'email', 'etal', 'ext-link', 'fn', 'on-behalf-of', 'role', 'uri', 'xref', 'x']
+let CONTRIB_GROUP = [
+  "contrib",
+  "address",
+  "aff",
+  "aff-alternatives",
+  "author-comment",
+  "bio",
+  "email",
+  "etal",
+  "ext-link",
+  "fn",
+  "on-behalf-of",
+  "role",
+  "uri",
+  "xref",
+  "x",
+  "genetics-comment",
+];
 
 export default {
+  type: "contrib-group",
+  tagName: "contrib-group",
 
-  type: 'contrib-group',
-  tagName: 'contrib-group',
-
-  import: function(el, node, converter) {
+  import: function (el, node, converter) {
     // node.id = 'contrib-group'; // there is only be one body element
-    node.xmlAttributes = el.getAttributes()
+    node.xmlAttributes = el.getAttributes();
 
-    let children = el.getChildren()
-    let iterator = new XMLIterator(children)
+    let children = el.getChildren();
+    let iterator = new XMLIterator(children);
 
-    iterator.oneOrMoreOf(CONTRIB_GROUP, function(child) {
-      node.nodes.push(converter.convertElement(child).id)
-    })
-    if (iterator.hasNext()) throw new Error('Illegal JATS: ' + el.outerHTML)
+    iterator.oneOrMoreOf(CONTRIB_GROUP, function (child) {
+      node.nodes.push(converter.convertElement(child).id);
+    });
+    if (iterator.hasNext()) throw new Error("Illegal JATS: " + el.outerHTML);
   },
 
-  export: function(node, el, converter) {
-    el.attr(node.xmlAttributes)
-    el.append(converter.convertNodes(node.nodes))
+  export: function (node, el, converter) {
+    el.attr(node.xmlAttributes);
+    el.append(converter.convertNodes(node.nodes));
     if (node.sigBlock) {
-      el.append(converter.convertNode(node.sigBlock))
+      el.append(converter.convertNode(node.sigBlock));
     }
-  }
-
-}
+  },
+};
