57b102fe1e859de0e4fc4570eebe70cf3535df08
nickstiffler <nicholasstiffle@gmail.com>
2023-11-17 14:34:27 -0800

    handle non-WB identifiers

--- client/src/app/markup/markup.service.ts
+++ client/src/app/markup/markup.service.ts
@@ -94,10 +94,9 @@ export class MarkupService {
 
     generateLink(prefix: string, externalModId: string, doi: string) {
         if (prefix == "WB" && !externalModId.startsWith("WB")) {
-            return "https://www.wormbase.org/species/c_elegans/strain/" + externalModId;
+            return "https://wormbase.org/search/all/" + externalModId;
         }
         if (prefix == "pombase") {
-
             return "https://identifiers.org/pombase:" + externalModId;
         }
         return 'https://identifiers.org/bioentitylink/' + prefix + ":" + externalModId + "?doi=" + doi;
